6cadc3c83d4cb6fc4e583e50255d07579e29578e
Cody Hatfield <cody.hatfield@me.com>
2021-01-08 14:13:46 -0800

    Truncate middle of licensee address

--- src/components/cards/ParcelInfo.js
+++ src/components/cards/ParcelInfo.js
@@ -28,6 +28,23 @@ const parcelQuery = gql`
   }
 `;
 
+function truncateStr(str, strLen) {
+  if (str.length <= strLen) {
+    return str;
+  }
+
+  var separator = "...";
+
+  var sepLen = separator.length,
+    charsToShow = strLen - sepLen,
+    frontChars = Math.ceil(charsToShow / 2),
+    backChars = Math.floor(charsToShow / 2);
+
+  return (
+    str.substr(0, frontChars) + separator + str.substr(str.length - backChars)
+  );
+}
+
 function ParcelInfo({
   account,
   adminContract,
@@ -181,7 +198,7 @@ function ParcelInfo({
         <>
           <p className="text-truncate">
             <span className="font-weight-bold">Licensee:</span>{" "}
-            {isLoading ? spinner : licenseOwner}
+            {isLoading ? spinner : truncateStr(licenseOwner, 11)}
           </p>
           <p>
             <span className="font-weight-bold">For Sale Price:</span>{" "}
