b5d14ada148c117ed6e54ccc3d6b4ca52cba9175
Cody Hatfield <cody.hatfield@me.com>
2021-09-09 10:59:37 -0700

    Change IPFS bootstrap node to WSS

--- pages/index.js
+++ pages/index.js
@@ -302,7 +302,14 @@ function IndexPageContent() {
           loadJsIpfsModule: () => require("ipfs-core"),
           options: {
             config: {
-              Bootstrap: [IPFS_BOOTSTRAP_PEER],
+              Bootstrap: [
+                IPFS_BOOTSTRAP_PEER,
+                "/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
+                "/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb",
+                "/dnsaddr/bootstrap.libp2p.io/p2p/QmZa1sAxajnQjVM8WjWXoMbmPd7NsWhfKsPkErzpm9wGkp",
+                "/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa",
+                "/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt",
+              ],
             },
             preload: {
               enabled: true,
