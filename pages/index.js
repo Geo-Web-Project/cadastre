6ebb440de0d4c092ee1cc3fdcab89b43e5c1f46f
Cody Hatfield <cody.hatfield@me.com>
2021-09-08 17:47:49 -0700

    Use Geo Web preload nodes and bootstrap

--- pages/index.js
+++ pages/index.js
@@ -300,22 +300,15 @@ function IndexPageContent() {
         }),
         jsIpfs({
           loadJsIpfsModule: () => require("ipfs-core"),
-          // config: {
-          //   Bootstrap: [
-          //     IPFS_BOOTSTRAP_PEER,
-          //     "/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
-          //     "/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb",
-          //     "/dnsaddr/bootstrap.libp2p.io/p2p/QmZa1sAxajnQjVM8WjWXoMbmPd7NsWhfKsPkErzpm9wGkp",
-          //     "/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa",
-          //     "/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt",
-          //   ],
-          // },
-          // options: {
-          //   preload: {
-          //     enabled: true,
-          //     addresses: [IPFS_PRELOAD_NODE],
-          //   },
-          // },
+          options: {
+            config: {
+              Bootstrap: [IPFS_BOOTSTRAP_PEER],
+            },
+            preload: {
+              enabled: true,
+              addresses: [IPFS_PRELOAD_NODE],
+            },
+          },
         }),
       ],
     });
