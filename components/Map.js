63c58aa0e1c2c1e9f58b685a393fb7111a83bffd
10dimensions <38999489+10dimensions@users.noreply.github.com>
2021-10-10 14:59:20 +0530

    Update Map.js

--- components/Map.js
+++ components/Map.js
@@ -168,7 +168,7 @@ function Map({
     ) {
       updateGrid(viewport.latitude, viewport.longitude, grid, setGrid);
     }
-  }, []);
+  });
 
   // if using Geocoder default settings, you can just use handleViewportChange directly
   const _onGeocoderViewportChange = useCallback((newViewport) => {
