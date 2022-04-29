abe6c73fde8f099e96e21c65712e0221fa47bb31
Cody Hatfield <cody.hatfield@me.com>
2022-04-29 09:44:09 -0700

    More typescript conversions

---
+++ components/map-style.ts
@@ -0,0 +1,48 @@
+import { LayerProps } from "react-map-gl";
+
+export const gridLayer: LayerProps = {
+  id: "grid-layer",
+  type: "fill",
+  paint: {
+    "fill-color": "#FFFFFF",
+    "fill-outline-color": "#000000",
+    "fill-opacity": 0.2,
+  },
+};
+export const gridHighlightLayer: LayerProps = {
+  id: "grid-highlight-layer",
+  type: "fill",
+  source: "grid-layer",
+  paint: {
+    "fill-color": "#FFFFFF",
+    "fill-outline-color": "#000000",
+    "fill-opacity": 0.5,
+  },
+};
+export function claimLayer(isValid: boolean): LayerProps {
+  return {
+    id: "claim-layer",
+    type: "fill",
+    source: "grid-layer",
+    paint: {
+      "fill-color": isValid ? "#FAFF00" : "#E11515",
+      "fill-opacity": 0.75,
+    },
+  };
+}
+export const parcelLayer: LayerProps = {
+  id: "parcels-layer",
+  type: "fill",
+  paint: {
+    "fill-color": "#2FC1C1",
+    "fill-opacity": 0.5,
+  },
+};
+export const parcelHighlightLayer: LayerProps = {
+  id: "parcels-highlight-layer",
+  type: "fill",
+  paint: {
+    "fill-color": "#2FC1C1",
+    "fill-opacity": 1.0,
+  },
+};
---
+++ components/sources/GridSource.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import { Source, Layer } from "react-map-gl";
+import { gridLayer } from "../map-style.js";
+
+type Props = {
+  isGridVisible: boolean;
+  grid: any | null;
+};
+
+function GridSource(props: Props) {
+  const { isGridVisible, grid } = props;
+  let gridFeatures = [];
+  if (grid != null && isGridVisible) {
+    gridFeatures = grid.features;
+  }
+
+  return (
+    <>
+      {grid != null ? (
+        <Source
+          id="grid-data"
+          type="geojson"
+          data={{
+            type: "FeatureCollection",
+            features: gridFeatures,
+          }}
+        >
+          <Layer {...gridLayer} />
+        </Source>
+      ) : null}
+    </>
+  );
+}
+
+export default GridSource;
