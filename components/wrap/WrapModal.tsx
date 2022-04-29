e787d6278bd4beb655b782c19d83df12a0d93568
Yadong Zhang <zhyd007@gmail.com>
2022-04-29 09:42:39 +0800

    fix: removed dummy code.

--- components/wrap/WrapModal.tsx
+++ components/wrap/WrapModal.tsx
@@ -86,23 +86,6 @@ function WrapMpdal({ account, signer, show, handleClose }: any) {
   }
 
   return (
-    <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
-  );
-
-  return (
     <Modal
       show={show}
       onHide={handleClose}
