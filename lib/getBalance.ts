7916ec1f8bcc7b0dd503cf71682c75aa254114c1
Yadong Zhang <zhyd007@gmail.com>
2022-05-13 16:10:52 +0800

    refactor: removed unused getETHxBalance utils.

--- lib/getBalance.ts
+++ lib/getBalance.ts
@@ -1,6 +1,4 @@
-import { Framework } from "@superfluid-finance/sdk-core";
 import { ethers } from "ethers";
-import { NETWORK_ID } from "./constants";
 import { formatBalance } from "./formatBalance";
 
 export const getETHBalance = async (
@@ -12,26 +10,4 @@ export const getETHBalance = async (
   const ethBalance = formatBalance(balance);
 
   return ethBalance;
-};
-
-export const getETHxBalance = async (
-  provider: ethers.providers.Web3Provider,
-  account: string,
-  paymentTokenAddress: string
-): Promise<string> => {
-  const sf = await Framework.create({
-    chainId: NETWORK_ID,
-    provider,
-  });
-
-  const ETHx = await sf.loadSuperToken(paymentTokenAddress);
-
-  const { availableBalance } = await ETHx.realtimeBalanceOf({
-    account,
-    providerOrSigner: provider as any,
-  });
-
-  const ETHxBalance = formatBalance(availableBalance);
-
-  return ETHxBalance;
 };
