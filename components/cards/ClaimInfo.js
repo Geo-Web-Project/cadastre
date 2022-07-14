9ce27baeaee85a1bb0994d54a6294a159a4b6ab7
tnrdd <tnrdd@tutanota.com>
2022-07-14 13:04:16 +0100

    update ClaimInfo view

--- components/cards/ClaimInfo.js
+++ components/cards/ClaimInfo.js
@@ -1,18 +1,38 @@
 import * as React from "react";
 import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import Image from "react-bootstrap/image";
+import { STATE } from "../Map";
+import { MAX_PARCEL_CLAIM } from "../../lib/constants";
 
-function ClaimInfo() {
+function ClaimInfo({ parcelClaimSize, setInteractionState }) {
   return (
     <Card className="bg-purple text-light">
-      <Card.Header>Claim Instructions</Card.Header>
       <Card.Body>
-        <Card.Text>
-          Single-click again to set your claim shape.
-        </Card.Text>
-        <Card.Text>
-          Click and drag the map to pan, if needed.
-        </Card.Text>
-        <Card.Text>Hit ESC to exit claim mode.</Card.Text>
+        <Card.Text className="d-flex align-items-center">
+          <Image
+            style={{
+              width: "2rem",
+            }}
+            src="grid.svg"
+          />
+          <span
+            className={`text-${
+              parcelClaimSize > MAX_PARCEL_CLAIM ? "danger" : "primary"
+            }`}
+          >
+            {parcelClaimSize}/{MAX_PARCEL_CLAIM} Maximum Coordinates
+          </span>
+          </Card.Text>
+        <Card.Text>Single click again to set your parcel shape.</Card.Text>
+        <Card.Text>Click and drag the map to pan, if needed.</Card.Text>
+        <Button
+          variant="danger"
+          className="w-100 mb-2"
+          onClick={() => setInteractionState(STATE.VIEWING)}
+        >
+          Cancel
+        </Button>
       </Card.Body>
     </Card>
   );
