003698f78e25add0a850116ea7eee37152664bb6
Cody Hatfield <cody.hatfield@me.com>
2022-10-25 16:31:39 -0700

    Update subgraph

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,7 +3,7 @@ export const NETWORK_NAME = "goerli";
 export const PAYMENT_TOKEN = "ETHx";
 export const PAYMENT_TOKEN_FAUCET_URL = "https://faucet.paradigm.xyz";
 export const SUBGRAPH_URL =
-  "https://api.thegraph.com/subgraphs/id/QmTd7pbXzRHDNPyfrHdNdhGT6rhs69EAV558N7EV79k4Lt";
+  "https://api.thegraph.com/subgraphs/id/QmV6V3i82f9biFQhY5ey1oifBmTT8X6X8Pov8QysoqWDmj";
 
 export const CERAMIC_URL = "https://ceramic-clay.geoweb.network/";
 export const CONNECT_NETWORK = "testnet-clay";
