de0a38dc09360d79c16a9311ddc6d4f8cd065e53
Cody Hatfield <cody.hatfield@me.com>
2022-12-12 14:44:08 -0800

    Add dotenv

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
+const webpack = require("webpack");
+const { parsed: myEnv } = require("dotenv").config({
+  path: `.env.${process.env.APP_ENV}`,
+});
+
+console.log(`Loading env from .env.${process.env.APP_ENV}`);
+
 module.exports = {
+  webpack(config) {
+    config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
+    return config;
+  },
   typescript: {
     ignoreBuildErrors: false,
   },
