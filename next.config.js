8ddac9da6ff62b204263f7b0f1812dc0bb6419c5
Cody Hatfield <cody.hatfield@me.com>
2022-12-13 08:37:09 -0800

    Add check for .env

--- next.config.js
+++ next.config.js
@@ -3,11 +3,12 @@ const { parsed: myEnv } = require("dotenv").config({
   path: `.env.${process.env.APP_ENV}`,
 });
 
-console.log(`Loading env from .env.${process.env.APP_ENV}`);
-
 module.exports = {
   webpack(config) {
-    config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
+    if (process.env.APP_ENV !== undefined) {
+      console.log(`Loading env from .env.${process.env.APP_ENV}`);
+      config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
+    }
     return config;
   },
   typescript: {
