4bcc4640a7dc383578d0bc079ed67ea8cf100fe6
tnrdd <tnrdd@tutanota.com>
2023-03-08 20:37:16 +0100

    feat: add parcel chat (#397)

--- dec.d.ts
+++ dec.d.ts
@@ -2,3 +2,4 @@ declare module "react-map-gl-geocoder";
 declare module "ipfs-provider";
 declare module "secure-random";
 declare module "as-geo-web-coordinate"
+declare module "@orbisclub/components";
