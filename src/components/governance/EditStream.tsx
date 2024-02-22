2fa8bd08de31dd5ce6e7e91ccb5858b9d6f8e3b0
tnrdd <tnrdd@tutanota.com>
2024-02-22 08:32:03 +0100

    feat(governance): change DAI on ramp to DAIx

--- src/components/governance/EditStream.tsx
+++ src/components/governance/EditStream.tsx
@@ -361,7 +361,7 @@ export default function EditStream(props: EditStreamProps) {
         ).toString()
       );
     }
-  }, [amountPerTimeInterval, timeInterval]);
+  }, [amountPerTimeInterval, timeInterval, liquidationEstimate]);
 
   const handleAmountStepping = (stepping: { increment: boolean }) => {
     const { increment } = stepping;
@@ -757,13 +757,13 @@ export default function EditStream(props: EditStreamProps) {
                     </Card.Text>
                     <Button
                       variant="link"
-                      href="https://swap.defillama.com/?chain=optimism&from=0x0000000000000000000000000000000000000000&to=0xda10009cbd5d07dd0cecc66161fc93d7c9000da1"
+                      href="https://jumper.exchange/?fromChain=10&fromToken=0x0000000000000000000000000000000000000000&toChain=10&toToken=0x7d342726B69C28D942ad8BfE6Ac81b972349d524"
                       target="_blank"
                       rel="noreferrer"
                       className="d-flex justify-content-center gap-1 bg-primary text-decoration-none rounded-3 text-white fs-4"
                     >
                       <Image src={SwapIcon} alt="swap" width={18} />
-                      Get DAI
+                      Get DAIx
                     </Button>
                   </Stack>
                 </Stack>
